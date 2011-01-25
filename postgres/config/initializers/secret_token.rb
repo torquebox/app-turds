402e1fcb608464afdeb54b847e204cbfeccf4df4
jcrossley <jim@crossleys.org>
2011-01-24 23:06:05 -0500

    Created with TB template

---
+++ postgres/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Postgres::Application.config.secret_token = '31d83a966c1e4c8f0a1145ee6bf5986a6761202593116f763932ea3421109263e96472a022fc57fbd348b4f59973c896d92a5249ca10691e9035016e88359991'
