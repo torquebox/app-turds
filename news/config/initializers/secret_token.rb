61346770ffa144752510e52868e3cdabc433abec
Jim Crossley <jim@crossleys.org>
2011-04-21 11:49:02 -0400

    jruby -S rails new news -m /Users/jim/local/torquebox/share/rails/template.rb

---
+++ news/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+News::Application.config.secret_token = 'c1e55c45fdddb7747291c8258fa946a63beb754dbe8f9a54c974f6f9a564e45834d890d04baab44a1e5183049e1282c5d46f03db07c49a3ff909beed04bee86f'
