bdca1eb3c7311a6bef8dc64ea01b5c3d0bf29e8e
Jim Crossley <jim@crossleys.org>
2013-03-10 11:02:20 -0400

    No idea

--- sleepy/sleepy.rb
+++ sleepy/sleepy.rb
@@ -5,8 +5,8 @@ class Sleepy < Sinatra::Base
 
   get '/snooze/:duration' do
     duration = (params[:duration] || 1).to_i
-    puts "request #{duration}"
     Sleeper.new.doze(duration)
+    "received request to sleep for #{duration}s\n"
   end
 
 end
